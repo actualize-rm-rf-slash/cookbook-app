8fd4702eb968ad9d2d2415f0b7617be3d18ce2d5
Peter Jang <peter.x.jang@gmail.com>
2018-03-12 19:04:24 -0500

    Add custom json response using model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,34 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
+
+  def as_json
+    {
+      id: id,
+      title: title,
+      chef: chef,
+      ingredients: ingredients_list,
+      directions: directions_list,
+      created_at: friendly_created_at,
+      prep_time: friendly_prep_time
+    }
+  end
 end
