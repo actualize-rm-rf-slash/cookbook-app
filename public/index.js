472d878561c40ace5ae4b92a5f83dc1386942479
Peter Jang <peter.x.jang@gmail.com>
2018-04-08 10:48:54 -0500

    Use JavaScript web request to get and display recipe data from backend

--- public/index.js
+++ public/index.js
@@ -1,10 +1,19 @@
+/* global axios */
+
 var recipeTemplate = document.querySelector("#recipe-card");
 var recipeContainer = document.querySelector(".row");
 
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+// IN RUBY: response = Unirest.get("http://localhost:3000/v1/recipes")
+axios.get("http://localhost:3000/v1/recipes").then(function(response) {
+  var recipes = response.data;
+  console.log(recipes);
+
+  recipes.forEach(function(recipe) {
+    var recipeClone = recipeTemplate.content.cloneNode(true);
+    recipeClone.querySelector("card-title").innerText = recipe.title;
+    recipeClone.querySelector(".ingredients").innerText = recipe.ingredients;
+    recipeClone.querySelector(".directions").innerText = recipe.directions;
+    recipeClone.querySelector(".card-img-top").src = recipe.image;
+    recipeContainer.appendChild(recipeClone);
+  });
+});
